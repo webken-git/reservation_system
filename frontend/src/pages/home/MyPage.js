3bb61e82945dee63d1fac0f5f5cab5a43aab6ce8
simaharu <flower-harubom.outlook.jp>
2021-06-08 19:43:06 +0900

    6/8(tue.)

--- frontend/src/pages/home/MyPage.js
+++ frontend/src/pages/home/MyPage.js
@@ -2,7 +2,7 @@ import React from "react";
 // import SideBar from "../../components/sidebar/SideBar"
 // import Header from "../../components/header/Header"
 import Approval from "./Approval"
-import SideBarAndHeaderRoute from "../../components/sidebarandheaderoute/SideBarAndHeaderRoute"
+import SideBarAndHeaderRoute from "../../components/rooter/SideBarAndHeaderRoute"
 //import './mypage.scss'
 
 export const MyPage =()=> {
@@ -18,7 +18,7 @@ export const MyPage =()=> {
   //   </div>
   // )
   return (
-      <SideBarAndHeaderRoute path="/Approval" exact children={<Approval/>} />
+      <SideBarAndHeaderRoute path="/mypage" exact children={<MyPage/>} />
   )
 
 }
