71044d325195365d47977406bb3a59de638c43c1
suzukikarin <karin200084@outlook.jp>
2021-06-08 18:53:54 +0900

    0608

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,6 +10,11 @@ import Home from "./pages/home/Home";
 import Login from "./pages/home/Login";
 import { MyPage }from "./pages/home/MyPage";
 import { ApprovalList } from "./pages/home/ApprovalList";
+import { UnapprovalList } from "./pages/home/UnapprovalList";
+import { DisapprovalList } from "./pages/home/DisapprovalList";
+import { CancelList } from "./pages/home/CancelList";
+import { UserList } from "./pages/home/UserList";
+
 function Menu() {
   return (
     <nav>
@@ -30,6 +35,10 @@ function App() {
           <GuestRoute path="/login" children={<Login />} />
           <PrivateRoute path="/mypage" children={<MyPage />} />
           <GuestRoute path="/approvallist" children={<ApprovalList />} />
+          <GuestRoute path="/unapprovallist" children={<UnapprovalList />} />
+          <GuestRoute path="/disapprovallist" children={<DisapprovalList />} />
+          <GuestRoute path="/cancellist" children={<CancelList />} />
+          <GuestRoute path="/userlist" children={<UserList />} />
         </Switch>
       </BrowserRouter>
     </Provider>
---
+++ frontend/src/pages/home/CancelList.js
@@ -0,0 +1,8 @@
+import React from "react";
+export const CancelList =()=> {
+  return (
+    <div>
+      <p>CancelList</p>
+    </div>
+  )
+}
