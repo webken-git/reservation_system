a60f88703e475a045e29399defa669f1c2bce9fd
kitaura-git <shrper56@gmail.com>
2021-12-24 02:32:44 +0900

    Bug fixes 2021-12-24

--- backend/django/reservations/models.py
+++ backend/django/reservations/models.py
@@ -158,6 +158,7 @@ class ApprovalApplication(models.Model):
       validators.MaxValueValidator(20000)],
       blank=True, null=True)
   conditions = models.TextField('承認の条件', max_length=255, blank=True, null=True)
+  cancellation_reason = models.TextField('キャンセルの理由', max_length=255, blank=True, null=True)
   approval = models.ForeignKey(
       Approval, verbose_name='approval',
       related_name='approval_app_approval',
