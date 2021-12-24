0856550565c9c0642ab111afe8e6d9e03a17a4a4
sasai <yuuki.rein@docomo.ne.jp>
2021-12-24 09:50:55 +0900

    12/24 approvalInfo修正

--- frontend/admin/src/components/approvalInfo/ApprovalInfo.js
+++ frontend/admin/src/components/approvalInfo/ApprovalInfo.js
@@ -7,10 +7,14 @@ import './approvalInfo.scss';
 const ApprovalInfo = (props) =>{
     const [reservation, setReservation] = useState([]);
     const [loading, setLoading] = useState(false);
-    // const [year, setYear] = useState("");
-    // const [month, setMonth] = useState("");
-    // const [date, setDate] = useState("");
+    const [year, setYear] = useState("");
+    const [month, setMonth] = useState("");
+    const [date, setDate] = useState("");
     const id = props.id;
+
+    // setYear(reservation.start.substr(0, 4));
+    // setMonth(reservation.start.substr(5, 2));
+    // setDate(reservation.start.substr(8, 2));
 
     const pullReservation = () => {
         setLoading(true);
@@ -29,62 +33,41 @@ const ApprovalInfo = (props) =>{
 
     useEffect(() => {
         pullReservation();
-        // setYear(reservation.start.substr(0, 2))
     }, [])
-    
-    return (
-    <>
-        <div className="info-wrapper">
-            <table className="info-table">
-                <tbody>
-                    <tr className="name-base">
-                        <td className="name-title">氏名：</td>
-                        {reservation.is_group === false ? (
-                            <td className="name-body">{reservation.reader_name}</td>
-                        ) : (
-                            <td className="name-body">{reservation.group_name}</td>
-                        )
-                        }
-                    </tr>
-                    <tr className="place">
-                        <td className="place-title">場所：</td>
-                        <td className="place-body">{reservation.place.name}</td>
-                    </tr>
-                    <tr className="time">
-                        <td className="time-title">時間：</td>
-                        <td className="time-body">{reservation.start}</td>
-                    </tr>
 
-                </tbody>
-            </table>
-        </div>
-        {loading && <Loading />}
-    </>
+    if(reservation.length === 0) {
+        return <Loading />;
+    } else {
+        return (
+            <>
+                <div className="info-wrapper">
+                    <table className="info-table">
+                        <tbody>
+                            <tr className="name-base">
+                                <td className="name-title">氏名：</td>
+                                {reservation.is_group === false ? (
+                                    <td className="name-body">{reservation.reader_name}</td>
+                                ) : (
+                                    <td className="name-body">{reservation.group_name}</td>
+                                )
+                                }
+                            </tr>
+                            <tr className="place">
+                                <td className="place-title">場所：</td>
+                                <td className="place-body">{reservation.place.name}</td>
+                            </tr>
+                            <tr className="time">
+                                <td className="time-title">時間：</td>
+                                <td className="time-body">{reservation.start}</td>
+                            </tr>
         
-    )
-
-    // const calendarType = props.calendarType;
-
-    // const dayOfWeek = props.date.getDay() ;
-    // const dayOfWeekStr = [ "日", "月", "火", "水", "木", "金", "土" ][dayOfWeek];
-
-    // if (calendarType === 'weekly') {
-    //     return (
-    //         <div className="head">
-    //             <p className="day">{props.day}</p>
-    //             <p className="date"><span className={(new Date(new Date().toDateString()).getTime()===new Date(props.date.toDateString()).getTime() ? "today" : "")}>{props.date.getDate()}</span></p>
-    //         </div>
-    //     )
-    // } else if (calendarType === 'daily') {
-    //     return (
-    //         <div className="daily-head">
-    //             <p className="day">{dayOfWeekStr}</p>
-    //             <p className="date"><span className={(new Date(new Date().toDateString()).getTime()===new Date(props.date.toDateString()).getTime() ? "today" : "")}>{props.date.getDate()}</span></p>
-    //         </div>
-    //     )
-    // }
-
-    
+                        </tbody>
+                    </table>
+                </div>
+                {loading && <Loading />}
+            </>
+        )
+    }    
 }
 
 export default ApprovalInfo;
